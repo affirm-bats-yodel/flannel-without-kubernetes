95ae2c6d8067fa44689769e41d841694e9c45ac7
Lee (李) <affirm-bats-yodel@duck.com>
2025-01-11 18:55:51 +0900

    chore: add yaml-language-server

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
           nativeBuildInputs = with pkgs; [
             marksman             # Markdown LSP
             bash-language-server # Bash Shell LSP
+            yaml-language-server # YAML LSP
             ansible              # Ansible for Automation
           ];
         };
